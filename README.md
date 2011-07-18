607ec4cba1113fedd6c87ed0fb29d707e5e11196
Blagovest Dachev <blago@dachev.com>
2011-07-17 20:19:57 -0500

    Simplified examples

--- README.md
+++ README.md
@@ -27,48 +27,13 @@ node web.js
 ## How to use
 
 ``` javascript
-var http     = require('http');
-var events   = require('events');
-var inherits = require('sys').inherits;
 var nquery   = require('nquery');
 
-function WebClient(host, path) {
-    var self       = this,
-        transport  = http.createClient(80, host),
-        request    = transport.request('GET', path, {'host': host});
+var html = '<html><head></head><body><div id="test" class="test">google</div></body></html>';
+var doc  = nquery.createHtmlDocument(html);
+var divs = doc('div.test');
 
-    request.end();
-
-    request.on('response', function (response) {
-        if (response.statusCode != 200) {
-            self.emit('done', response.statusCode, '');
-        }
-        else {
-            var html = '';
-
-            response.setEncoding('utf8');
-            response.on('data', function (chunk) {
-                html += chunk;
-            });
-            response.on('end', function (chunk) {
-                self.emit('done', 200, html);
-            });
-        }
-    });
+for (var i = 0; i < divs.length; i++) {
+    console.log(divs[i].toString());
 }
-inherits(WebClient, events.EventEmitter);
-
-var client = new WebClient('www.yahoo.com', '/');
-client.on('done', function(status, html) {
-    if (status != 200) {
-        throw 'unable to download page';
-    }
-
-    var doc  = nquery.createHtmlDocument(html);
-    var divs = doc('div');
-
-    for (var i = 0; i < divs.length; i++) {
-        console.log(divs[i].toString());
-    }
-});
 ```
--- examples/web.js
+++ examples/web.js
@@ -1,25 +1,24 @@
 #!/usr/bin/env node
 
-var http     = require('http'),
-    fs       = require('fs'),
-    events   = require('events'),
-    inherits = require('sys').inherits,
-    dominiq  = require('../lib/dominiq'),
-    sizzFact = require('../lib/node-sizzle');
+var http     = require('http');
+var events   = require('events');
+var inherits = require('sys').inherits;
+var nquery   = require('../lib/nquery');
 
 function WebClient(host, path) {
     var self       = this,
         transport  = http.createClient(80, host),
         request    = transport.request('GET', path, {'host': host});
-        
+
     request.end();
+
     request.on('response', function (response) {
         if (response.statusCode != 200) {
             self.emit('done', response.statusCode, '');
         }
         else {
             var html = '';
-            
+
             response.setEncoding('utf8');
             response.on('data', function (chunk) {
                 html += chunk;
@@ -32,42 +31,18 @@ function WebClient(host, path) {
 }
 inherits(WebClient, events.EventEmitter);
 
-/*
-function FileClient(path) {
-    var self = this;
-    
-    fs.readFile(path, 'utf8', function (err, html) {
-        if (err) {
-            self.emit('done', err, '');
-        }
-        else {
-            self.emit('done', 200, html);
-        }
-    });
-}
-inherits(FileClient, events.EventEmitter); 
-*/
-
-
 var client = new WebClient('www.yahoo.com', '/');
 client.on('done', function(status, html) {
     if (status != 200) {
         throw 'unable to download page';
     }
-    
-    var doc      = dominiq.createHTMLDocument(html),
-        window   = {document:doc},
-        sizzle   = sizzFact.sizzleInit(window),
-        divs     = sizzle('div');
-    
+
+    var doc  = nquery.createHtmlDocument(html);
+    var divs = doc('div');
+
     for (var i = 0; i < divs.length; i++) {
         console.log(divs[i].toString());
     }
 });
- 
-
-
-
-
 
 
